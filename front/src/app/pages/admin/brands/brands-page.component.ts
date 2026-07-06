662a1d42b38803f5b7c6270fbb24318853f39766
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-07-06 09:14:01 -0500

    categorias dash

--- front/src/app/pages/admin/brands/brands-page.component.ts
+++ front/src/app/pages/admin/brands/brands-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrandsService } from '../../../core/brands.service';
+import { ToastService } from '../../../core/toast.service';
 import { Marca } from '../../../models/catalog.models';
 
 @Component({
@@ -18,7 +19,7 @@ export class BrandsPageComponent implements OnInit {
   editingName = '';
   deleteConfirmId: number | null = null;
 
-  constructor(private brandsService: BrandsService) {}
+  constructor(private brandsService: BrandsService, private toast: ToastService) {}
 
   ngOnInit() {
     this.brandsService.brands$.subscribe(list => {
@@ -29,6 +30,7 @@ export class BrandsPageComponent implements OnInit {
   createBrand() {
     if (!this.newName.trim()) return;
     this.brandsService.createBrand(this.newName);
+    this.toast.success('Marca creada exitosamente');
     this.newName = '';
   }
 
@@ -40,6 +42,7 @@ export class BrandsPageComponent implements OnInit {
   saveEdit() {
     if (this.editingId !== null && this.editingName.trim()) {
       this.brandsService.updateBrand(this.editingId, this.editingName);
+      this.toast.success('Marca actualizada correctamente');
     }
     this.editingId = null;
     this.editingName = '';
@@ -57,6 +60,7 @@ export class BrandsPageComponent implements OnInit {
   executeDelete() {
     if (this.deleteConfirmId !== null) {
       this.brandsService.deleteBrand(this.deleteConfirmId);
+      this.toast.success('Marca eliminada del catálogo');
       this.deleteConfirmId = null;
     }
   }
